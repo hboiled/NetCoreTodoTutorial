a6be64a5885b17698c320a206804e07d1df42ee0
Sam Lee <sflee36@yahoo.com.au>
2020-03-29 13:33:20 +0800

    first views and models added

---
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index() 
+        {
+            // get to-do items from db
+
+
+            // put items into model
+
+
+            // render view using model
+            throw new NotImplementedException();
+        }
+    }
+}
