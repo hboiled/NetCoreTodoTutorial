f56d12d3594a59d81721c1cf693c3a2428c8fb4b
Sam Lee <sflee36@yahoo.com.au>
2020-03-30 09:01:32 +0800

    2nd commit

--- AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -3,21 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index() 
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index() 
         {
             // get to-do items from db
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
 
             // put items into model
-
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
             // render view using model
-            throw new NotImplementedException();
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                // if model is invalid, redirect back to (todo) index
+                return RedirectToAction("Index");
+            }
+
+            var success = await _todoItemService.AddItemAsync(newItem);
+
+            if (!success)
+            {
+                return BadRequest("Could not add item.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id) 
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var success = await _todoItemService.MarkDoneAsync(id);
+            
+            if (!success) 
+            {
+                return BadRequest("Could not mark item as done");
+            } 
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ AspNetCoreTodo/AspNetCoreTodo/Services/FakeTodoItemService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Filler Entry",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var item2 = new TodoItem
+            {
+                Title = "Filler Entry2",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+
+            return Task.FromResult(new[] {item1, item2});
+        }
+
+        public Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> MarkDoneAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
